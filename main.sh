4c0ddfca3e28216ecc94771233caab40a3446319
Ramesh <dev0pslearning2025@gmail.com>
2024-08-24 14:25:29 +0530

    myscripts

--- main.sh
+++ main.sh
@@ -4,14 +4,6 @@ today=$(date +"%Y-%m-%d")
 directory="/root/myscripts"
 destination="/root/myscripts/serverrotationfiles"
 
-# Function to display help message
-if [ ! -f "${destination}/ssl.conf_${today}" ]; then
-    cp -rp ${directory}/ssl.conf ${destination}/ssl.conf_${today}
-    echo "taken master copy"
-else
-    echo "master copy is already there"
-fi
-
 # Check if any arguments were passed
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 {server1|server2|all_in}"
@@ -22,12 +14,20 @@ fi
 case $1 in
     server1)
         # Get today's date in YYYY-MM-DD format
+        if [ ! -f "${destination}/ssl.conf_${today}" ]; then
+            cp -rp ${directory}/ssl.conf ${destination}/ssl.conf_${today}
+            echo "taken master copy"
+        fi
         sed -i '/edppvlpxya01:2033/s/^/# /' ${directory}/ssl.conf
         echo "server1 taken out"
         ;;
     
     server2)
         # Search for files modified in the last 7 days
+        if [ ! -f "${destination}/ssl.conf_${today}" ]; then
+            cp -rp ${directory}/ssl.conf ${destination}/ssl.conf_${today}
+            echo "taken master copy"
+        fi
         sed -i '/edppvlpxya02:2033/s/^/# /' ${directory}/ssl.conf
         echo "server2 taken out"
         ;;
