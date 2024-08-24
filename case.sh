8d33963b5295d5e3fc3824700ad8fb221336fe13
Ramesh <dev0pslearning2025@gmail.com>
2024-08-24 13:42:27 +0530

    myscripts

--- case.sh
+++ case.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 today=$(date +"%Y-%m-%d")
-directory=""
-destination=""
+directory="/root/myscripts"
+destination="/root/myscripts/serverrotationfiles"
 
 # Function to display help message
+if [ ! ssl.conf_${today} ]; then
+cp -rp ${directory}/ssl.conf ${destination}/ssl.conf_${today}
+    echo "taken master copy"
+else
+    echo "master copy is already there"
+fi
 
 # Check if any arguments were passed
 if [ $# -eq 0 ]; then
@@ -16,16 +22,19 @@ fi
 case $1 in
     server1)
         # Get today's date in YYYY-MM-DD format
-         sed -i '/edppvlpxya01:2033/s/^/# /' ssl.conf
-
+         sed -i '/edppvlpxya01:2033/s/^/# /' ${directory}/ssl.conf
+         echo "server1 taken out"
         ;;
     
     server2)
         # Search for files modified in the last 7 days
-         sed -i '/edppvlpxya02:2033/s/^/# /' ssl.conf
+         sed -i '/edppvlpxya02:2033/s/^/# /' ${directory}/ssl.conf
+         echo "server2 taken out"
         ;;
     
     all_in)
+        echo "all servers are in rotaion"
+        cp -rp ${destination}/ssl.conf_${today} ${directory}/ssl.conf 
         ;;
     
     *)
